c9cd0e019cdd92fa5e691a08808ab055b00f8910
Will <will@channel.io>
2020-10-19 00:14:46 +0900

    Add log out button on NavBar

--- app/src/App.js
+++ app/src/App.js
@@ -7,7 +7,6 @@ import GroupEdit from './pages/GroupEdit'
 import GroupList from './pages/GroupList';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-import Admin from './pages/Admin';
 import { AuthContext } from "./context/auth";
 
 class App extends Component {
@@ -41,7 +40,6 @@ class App extends Component {
             <Route path='/login' component={Login} />
             <Route path='/signup' component={Signup} />
             <PrivateRoute path='/' exact={true} component={Home} />
-            <PrivateRoute path='/admin' exact={true} component={Admin} />
             <PrivateRoute path='/groups' exact={true} component={GroupList} />
             <PrivateRoute path='/groups/:id' component={GroupEdit} />
           </Switch>
--- app/src/AppNavbar.js
+++ app/src/AppNavbar.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
-import { Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import { Button, Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { AuthContext } from './context/auth';
 
 export default class AppNavbar extends Component {
+    static contextType = AuthContext;
+
     constructor(props) {
         super(props);
         this.state = { isOpen: false };
         this.toggle = this.toggle.bind(this);
+        this.logOut = this.logOut.bind(this);
+    }
+
+    logOut() {
+        // remove authentication token
+        this.context.setAuthTokens();
     }
 
     toggle() {
@@ -21,6 +30,9 @@ export default class AppNavbar extends Component {
             <NavbarToggler onClick={this.toggle} />
             <Collapse isOpen={this.state.isOpen} navbar>
                 <Nav className="ml-auto" navbar>
+                    <NavItem>
+                        <Button onClick={this.logOut}>Log out</Button>
+                    </NavItem>
                     <NavItem>
                         <NavLink href="https://github.com/Parannarae">GitHub</NavLink>
                     </NavItem>
